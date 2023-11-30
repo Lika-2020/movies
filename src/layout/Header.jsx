8ffc562253dabb0d227dc276363f78acd2b1a052
Анжелика <lika-2020@yandex.ru>
2023-11-30 13:04:38 +0300

    add project movie

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/Lika-2020/movies">Repo</a></li>
                 </ul>
             </div>
         </nav>
