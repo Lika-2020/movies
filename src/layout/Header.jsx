8bf3f63bfb5d07aca2b8d256a4009716ba9b6d97
Анжелика <lika-2020@yandex.ru>
2023-11-30 13:32:08 +0300

    removing errors header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <nav className="#2196f3 blue">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Movies</a>
+                <a href=" " className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="https://github.com/Lika-2020/movies">Repo</a></li>
                 </ul>
